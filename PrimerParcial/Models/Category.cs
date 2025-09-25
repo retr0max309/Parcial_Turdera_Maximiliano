f583a4cfc17fe8a2d108862a3179ffc4127e9d3c
retr0max309 <maximilianoturderacolque@gmail.com>
2025-09-25 19:45:59 -0400

    ajustar Ingredient y Category

--- PrimerParcial/Models/Category.cs
+++ PrimerParcial/Models/Category.cs
@@ -1,21 +1,19 @@
-﻿using System.ComponentModel.DataAnnotations;
+﻿using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 
 namespace PrimerParcial.Models
 {
     public class Category
     {
-        // Clave Primaria (PK)
         public int Id { get; set; }
 
-        [Required]
-        [StringLength(50)]
-        public string Name { get; set; } // Ejemplo: "Postres", "Platos Principales"
+        [Required, StringLength(50)]
+        public string Name { get; set; } = ""; 
 
-        public string Description { get; set; }
+        [StringLength(200)]
+        public string? Description { get; set; }
 
-        // --- Relaciones de Entity Framework Core ---
-
-        // Propiedad de Navegación: El lado 'muchos' para las Recetas en esta Categoría
-        public ICollection<Recipe> Recipes { get; set; }
+        
+        public List<Recipe> Recipes { get; set; } = new();
     }
 }
--- PrimerParcial/Models/Ingredient.cs
+++ PrimerParcial/Models/Ingredient.cs
@@ -4,21 +4,19 @@ namespace PrimerParcial.Models
 {
     public class Ingredient
     {
-        // Clave Primaria (PK)
         public int Id { get; set; }
 
+        [Required, StringLength(120)]
+        public string Name { get; set; } = ""; 
+
+        [Required, StringLength(60)]
+        public string Quantity { get; set; } = ""; 
+
+        
         [Required]
-        public string Name { get; set; } // Ejemplo: "Harina de Trigo"
-
-        [Required]
-        public string Quantity { get; set; } // Ejemplo: "2 tazas" o "500 gramos"
-
-        // --- Relaciones de Entity Framework Core ---
-
-        // Clave Foránea (FK): Vincula este ingrediente a la receta
         public int RecipeId { get; set; }
 
-        // Propiedad de Navegación: El lado 'uno'
-        public Recipe Recipe { get; set; }
+        
+        public Recipe? Recipe { get; set; }
     }
 }

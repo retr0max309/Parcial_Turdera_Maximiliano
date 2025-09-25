68b49bb0a7f745d9cd243e3015d1d5afbd31b465
retr0max309 <maximilianoturderacolque@gmail.com>
2025-09-25 19:39:16 -0400

    agregar modelo Recipe y relaciones básicas con Category y Ingredient

---
+++ PrimerParcial/Models/Recipe.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+
+namespace PrimerParcial.Models
+{
+    public class Recipe
+    {
+        public int Id { get; set; }
+
+        [Required, StringLength(120)]
+        public string Name { get; set; } = "";
+
+        [StringLength(500)]
+        public string? Description { get; set; }
+
+        // Relación con Category 
+        [Required]
+        public int CategoryId { get; set; }
+        public Category? Category { get; set; }
+
+        // Relación con Ingredient 
+        public List<Ingredient> Ingredients { get; set; } = new();
+    }
+}
